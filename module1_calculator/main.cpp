75136cc1920e14c3490e540fb6ba2f20651de755
Michael Munoz <mmunoz00@byui.edu>
2024-01-22 15:04:14 -0700

    Project Start

---
+++ module1_calculator/main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "This is a demonstration of a Calculator.";
+    cout << "========================================";
+    cout << "Demonstrate Addition (5 + 4 = 9)";
+    cout << addition();
+
+    cout << "Demonstrate Subtraction (10 - 3 = 7)";
+    cout << subtration();
+
+    cout << "Demonstrate Multiplication (3 * 2 = 6)";
+    cout << multiplication();
+
+    cout << "Demonstrate Division (8 / 4 = 2)";
+    cout << division();
+
+    return 0;
+}
+
+int addition() {
+
+}
+
+int subtration() {
+
+}
+
+int multiplication() {
+
+}
+
+int division() {
+    
+}
