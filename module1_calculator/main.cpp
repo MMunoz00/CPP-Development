521c0bad989aec635d937cc66a09038d17dfc7a8
Michael Munoz <mmunoz00@byui.edu>
2024-01-22 15:25:13 -0700

    Reordered Functions

--- module1_calculator/main.cpp
+++ module1_calculator/main.cpp
@@ -1,24 +1,6 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-    cout << "This is a demonstration of a Calculator.";
-    cout << "========================================";
-    cout << "Demonstrate Addition (5 + 4 = 9)";
-    cout << addition();
-
-    cout << "Demonstrate Subtraction (10 - 3 = 7)";
-    cout << subtration();
-
-    cout << "Demonstrate Multiplication (3 * 2 = 6)";
-    cout << multiplication();
-
-    cout << "Demonstrate Division (8 / 4 = 2)";
-    cout << division();
-
-    return 0;
-}
-
 int addition(int opA, int opB) {
     int value;
 
@@ -49,4 +31,21 @@ int division(int opA, int opB) {
     value = opA / opB;
 
     return value;
+}
+int main() {
+    cout << "This is a demonstration of a Calculator.";
+    cout << "========================================";
+    cout << "Demonstrate Addition (5 + 4 = 9)";
+    cout << addition(5, 4);
+
+    cout << "Demonstrate Subtraction (10 - 3 = 7)";
+    cout << subtration(10, 3);
+
+    cout << "Demonstrate Multiplication (3 * 2 = 6)";
+    cout << multiplication(3, 2);
+
+    cout << "Demonstrate Division (8 / 4 = 2)";
+    cout << division(8, 4);
+
+    return 0;
 }
